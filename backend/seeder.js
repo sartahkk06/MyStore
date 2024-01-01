de27d6031d7601d270e7e6967c0e3ee456f692ca
Sarthak Nandvikar <nandvikarsarthak@gmail.com>
2024-01-02 00:17:13 +0530

    Final Commit

--- backend/seeder.js
+++ backend/seeder.js
@@ -12,44 +12,44 @@ dotenv.config();
 connectDB();
 
 const importData = async () => {
-  try {
-    await Order.deleteMany();
-    await Product.deleteMany();
-    await User.deleteMany();
+	try {
+		await Order.deleteMany();
+		await Product.deleteMany();
+		await User.deleteMany();
 
-    const createdUsers = await User.insertMany(users);
-    const adminUser = createdUsers[0]._id;
+		const createdUsers = await User.insertMany(users);
+		const adminUser = createdUsers[0]._id;
 
-    const sampleProducts = products.map((product) => {
-      return { ...product, user: adminUser };
-    });
+		const sampleProducts = products.map((product) => {
+			return { ...product, user: adminUser };
+		});
 
-    await Product.insertMany(sampleProducts);
+		await Product.insertMany(sampleProducts);
 
-    console.log('Data imported'.green.inverse);
-    process.exit();
-  } catch (err) {
-    console.error(`${err}`.red.inverse);
-    process.exit(1);
-  }
+		console.log('Data imported'.green.inverse);
+		process.exit();
+	} catch (err) {
+		console.error(`${err}`.red.inverse);
+		process.exit(1);
+	}
 };
 
 const destroyData = async () => {
-  try {
-    await Order.deleteMany();
-    await Product.deleteMany();
-    await User.deleteMany();
+	try {
+		await Order.deleteMany();
+		await Product.deleteMany();
+		await User.deleteMany();
 
-    console.log('Data destroyed'.red.inverse);
-    process.exit();
-  } catch (err) {
-    console.error(`${err}`.red.inverse);
-    process.exit(1);
-  }
+		console.log('Data destroyed'.red.inverse);
+		process.exit();
+	} catch (err) {
+		console.error(`${err}`.red.inverse);
+		process.exit(1);
+	}
 };
 
 if (process.argv[2] === '-d') {
-  destroyData();
+	destroyData();
 } else {
-  importData();
+	importData();
 }
--- backend/server.js
+++ backend/server.js
@@ -26,26 +26,24 @@ const __dirname = path.resolve();
 app.use(`/uploads`, express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+	});
 } else {
-  app.get('/', (req, res) => {
-    res.send('API is running...');
-  });
+	app.get('/', (req, res) => {
+		res.send('API is running...');
+	});
 }
-
-console.log(app);
 
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  );
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+	);
 });
